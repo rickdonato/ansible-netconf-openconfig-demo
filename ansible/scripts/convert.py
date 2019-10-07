cd482465e334e7a484164427b35a1d6230c01813
packetpimp3 <admin@packetflow.co.uk>
2019-10-07 12:04:26 +0100

    updates

--- ansible/scripts/convert.py
+++ ansible/scripts/convert.py
@@ -1,44 +1,54 @@
 #!./venv/bin/python -i
+"""
+Author: Rick Donato <admin@packetflow.co.uk>
 
-from ydk.providers import CodecServiceProvider
-from ydk.services import CodecService
-from copy import deepcopy
+Script to convert JSON/YAML to XML structures.
+"""
+
 import argparse
 import json
+
 import yaml
-import sys
+from ydk.providers import CodecServiceProvider
+from ydk.services import CodecService
 
-parser = argparse.ArgumentParser(description='convert yang json to xml')
-parser.add_argument('-j','--json_file', help='json_file')
-parser.add_argument('-y','--yaml_file', help='yaml_file')
+PARSER = argparse.ArgumentParser(description='convert yang json to xml')
+PARSER.add_argument('-j', '--json_file', help='json_file')
+PARSER.add_argument('-y', '--yaml_file', help='yaml_file')
 
-args = vars(parser.parse_args())
+ARGS = vars(PARSER.parse_args())
 
-codec = CodecService()
+CODEC = CodecService()
 
-json_provider = CodecServiceProvider(type='json')
-xml_provider = CodecServiceProvider(type='xml')
+JSON_PROVIDER = CodecServiceProvider(type='json')
+XML_PROVIDER = CodecServiceProvider(type='xml')
+
 
 def yang_yaml_to_xml(yaml_file):
+    """
+    Convert YAML to XML
+    """
     with open(yaml_file, 'r') as yaml_in:
-        yaml_object = yaml.safe_load(yaml_in) 
-    
-    config_json_yang = json.dumps(yaml_object)
-    decoded_json_yang = codec.decode(json_provider, config_json_yang)
+        yaml_object = yaml.safe_load(yaml_in)
 
-    yang_xml = codec.encode(xml_provider, decoded_json_yang)
+    config_json_yang = json.dumps(yaml_object)
+    decoded_json_yang = CODEC.decode(JSON_PROVIDER, config_json_yang)
+
+    yang_xml = CODEC.encode(XML_PROVIDER, decoded_json_yang)
     return yang_xml
+
 
 def yang_json_to_xml(json_file):
+    """
+    Convert JSON to XML.
+    """
     config_json_yang = json.dumps(json.load(open(json_file)))
-    
-    decoded_json_yang = codec.decode(json_provider, config_json_yang)
-    
-    yang_xml = codec.encode(xml_provider, decoded_json_yang)
+    decoded_json_yang = CODEC.decode(JSON_PROVIDER, config_json_yang)
+
+    yang_xml = CODEC.encode(XML_PROVIDER, decoded_json_yang)
     return yang_xml
 
-
-if args['json_file']:
-    print(yang_json_to_xml(args['json_file']))
-elif args['yaml_file']:
-    print(yang_yaml_to_xml(args['yaml_file']))
+if ARGS['json_file']:
+    print(yang_json_to_xml(ARGS['json_file']))
+elif ARGS['yaml_file']:
+    print(yang_yaml_to_xml(ARGS['yaml_file']))
